d0f813910c6f0c6bf886eb9b6906ceb0dce8dc42
Ureimu <steam1090693441@163.com>
2023-04-27 15:47:02 +0800

    feat: add read multiple data files example

--- pytorch/data.py
+++ pytorch/data.py
@@ -1,5 +1,15 @@
 import json
+import os
 
-file = open(r"../openposeOutput/json/COCO_val2014_000000000192_keypoints.json", mode='r')
-data = json.load(file)
-print(data["people"][0]["pose_keypoints_2d"])
+dataDir = r"../openposeOutput/json"
+dataNameList = os.listdir(dataDir)
+print(dataNameList)
+objectDataList = []
+for fileName in dataNameList:
+    file = open(rf"{dataDir}/{fileName}", mode='r')
+    data = json.load(file)
+    if len(data["people"])>0:
+        objectDataList.append(data["people"][0]["pose_keypoints_2d"])
+
+print(f"总数据文件数：{len(dataNameList)}，有效数据文件数：{len(objectDataList)}")
+print(objectDataList)
