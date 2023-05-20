08ff255c834eafe62417c9480f082f27d0b64006
Ureimu <steam1090693441@163.com>
2023-05-20 19:48:16 +0800

    feat: merge readMark function to readData.py

--- pytorch/fullyConnectedNetwork.py
+++ pytorch/fullyConnectedNetwork.py
@@ -2,79 +2,147 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
 import readData
 import numpy
 
-from pytorch.readMark import readMark
+from pytorch.readData import readMark
+
+networkDepth = 3
+trainSetRate = 0.7
+outputTensorLength = 7
+learning_rate = 0.1
+epochNum = 10000
+epochReportPercentage = 0.1
+MODEL_STORE_PATH = "./model"
+
+poseData = readData.readPoseData("../openposeOutput/train/json")
+print(poseData)
+singlePoseDataLength = len(poseData[0])
+markMatrix, markList = readMark("./mark.txt")
+splitLimit = int(len(markMatrix) * trainSetRate)
+trainMatrix = markMatrix[0:splitLimit]
+trainMarkList = markList[0:splitLimit]
+testMatrix = markMatrix[splitLimit:len(markMatrix)]
+testMarkList = markList[splitLimit:len(markMatrix)]
+print(f"poseLength:{singlePoseDataLength}, trainDataLength:{splitLimit}, testDataLength:{len(markMatrix) - splitLimit}")
 
 
 # 定义模型
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(75, 50)
-        self.fc2 = nn.Linear(50, 25)
-        self.fc3 = nn.Linear(25, 7)
+        intervalList = [i / networkDepth for i in range(1, networkDepth + 1)]
+        intervalList.reverse()
+        intervalPercentageStepIncrement = 1 / networkDepth
+        startEndDelta = singlePoseDataLength - outputTensorLength
+        self.fcList = [nn.Linear(int(startEndDelta * intervalPercentage + outputTensorLength),
+                                 int(startEndDelta * (
+                                         intervalPercentage - intervalPercentageStepIncrement) + outputTensorLength))
+                       for
+                       intervalPercentage in intervalList]
+        self.lastFc = self.fcList.pop()
 
     def forward(self, netX):
-        netX = torch.relu(self.fc1(netX))
-        netX = torch.relu(self.fc2(netX))
-        netX = self.fc3(netX)
+        for fc in self.fcList:
+            netX = torch.relu(fc(netX))
+        netX = self.lastFc(netX)
         return netX
 
 
 # 定义一个空列表，用于存储每一次迭代的损失函数值
 losses = []
+accList = []
+accXList = []
+accTestList = []
 
 net = Net()
-markMatrix, markList = readMark("./mark.txt")
-splitLimit = int(len(markMatrix)*0.7)
-trainMatrix = markMatrix[0:splitLimit]
-trainMarkList = markList[0:splitLimit]
-testMatrix = markMatrix[splitLimit:len(markMatrix)]
-testMarkList = markList[splitLimit:len(markMatrix)]
 
 # 定义数据
-x = torch.tensor(readData.readPoseData("../openposeOutput/train/json")[0:splitLimit], dtype=torch.float)
+x = torch.tensor(poseData[0:splitLimit], dtype=torch.float)
 y = torch.tensor(trainMatrix, dtype=torch.float)
-xTest = torch.tensor(readData.readPoseData("../openposeOutput/train/json")[splitLimit:len(markMatrix)], dtype=torch.float)
-print(y)
+xTest = torch.tensor(poseData[splitLimit:len(markMatrix)],
+                     dtype=torch.float)
+yTest = torch.tensor(testMatrix, dtype=torch.float)
+# print(y)
 
 # 定义损失函数和优化器
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.1)
+optimizer = optim.SGD(net.parameters(), lr=learning_rate)
 
 # 训练模型
-for epoch in range(1500):
+
+for epoch in range(1, epochNum + 1):
     optimizer.zero_grad()
     outputs = net(x)
     loss = criterion(outputs, y.argmax(dim=1))
-    losses.append(loss.item())
+    lossNum = loss.item()
+    losses.append(lossNum)
     loss.backward()
     optimizer.step()
 
+    if epoch % int(epochNum * epochReportPercentage) == 0 or epoch == epochNum:
+        # print(outputs)
+        outClass = torch.argmax(outputs, 1)
+        yClass = torch.argmax(y, 1)
+        # print(outClass,y)
+        acc = sum([1 if yClass[i] == outClass[i] else 0 for i in range(len(y))]) / len(y)
+        accList.append(acc)
+        accXList.append(epoch)
+        print(f"epoch:{epoch} {'%3.2f' % (epoch / epochNum * 100)}% loss:{lossNum} acc:{acc}")
+
+        # Evaluate test set accuracy
+        test_outputs = net(xTest)
+        test_outClass = torch.argmax(test_outputs, 1)
+        test_yClass = torch.argmax(yTest, 1)
+        test_acc = sum([1 if test_yClass[i] == test_outClass[i] else 0 for i in range(len(yTest))]) / len(yTest)
+        accTestList.append(test_acc)
+        print(f"Test set accuracy: {test_acc}")
+
+
 # 绘制损失函数值随着迭代次数的变化图表
-plt.plot(losses)
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
+fig = plt.figure(figsize=(7, 4))
+ax1 = fig.add_subplot(111)
+ax1.plot(losses, color="r", label="loss")
+ax1.set_ylabel("loss")
+ax1.set_xlabel("epoch")
+
+# 绘制在训练集上的准确率随着迭代次数的变化图表
+ax2 = ax1.twinx()
+ax2.plot(accXList, accList, color="b", label="acc")
+ax2.plot(accXList, accTestList, color="g", label="acc test")
+ax2.set_xlabel("Same")
+ax2.set_ylabel("accuracy on train set")
+
+
+def percentageFormatter(tmp, pos):
+    return '%3.1f' % (tmp * 100) + "%"
+
+
+ax2.yaxis.set_major_formatter(FuncFormatter(percentageFormatter))
+
 plt.show()
+
+# print(output_tensor)
 
 with torch.no_grad():
     output_tensor = net(x)
     pred_probab = nn.Softmax(dim=1)(output_tensor)
     y_pred = pred_probab.argmax(1)
-    print(f"trainList Predicted class: {y_pred}")
-    print(f"trainList mark class: {trainMarkList}")
-    print(f"accuracy: {sum([1 if y_pred[i]==trainMarkList[i] else 0 for i in range(len(y_pred))])/len(y_pred)}")
+    # print(f"trainList Predicted class: {y_pred}")
+    # print(f"trainList mark class: {trainMarkList}")
+    print(
+        f"trainSet accuracy: {sum([1 if y_pred[i] == trainMarkList[i] else 0 for i in range(len(y_pred))]) / len(y_pred)}")
 
     output_tensor = net(xTest)
     pred_probab = nn.Softmax(dim=1)(output_tensor)
     y_pred = pred_probab.argmax(1)
     print(f"testList Predicted class: {y_pred}")
     print(f"testList mark class: {testMarkList}")
-    print(f"accuracy: {sum([1 if y_pred[i] == testMarkList[i] else 0 for i in range(len(y_pred))]) / len(y_pred)}")
-
-print(output_tensor)
+    print(
+        f"testSet accuracy: {sum([1 if y_pred[i] == testMarkList[i] else 0 for i in range(len(y_pred))]) / len(y_pred)}")
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
+print(f"training finished on {device}")
+
+torch.save(net.state_dict(), f'{MODEL_STORE_PATH}/net_model.ckpt')
--- pytorch/readData.py
+++ pytorch/readData.py
@@ -2,6 +2,31 @@ import json
 import os
 from typing import List
 
+# range(25)
+pickList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 15, 16, 17, 18]
+
+ListLength = 3 * len(pickList)
+
+from typing import List
+import torch
+
+markDict = {
+    " ": 0,  # 无法识别的姿势
+    "q": 1,  # 正确的坐姿
+    "e": 2,  # 头部倾斜
+    "w": 3,  # 身体前倾
+    "s": 4,  # 身体后倾
+    "a": 5,  # 身体右倾（对应图片中为左倾）
+    "d": 6  # 身体左倾（对应图片中为右倾）
+}
+
+
+# len(dataList)x11x3
+def sfPick(listHere):
+    return [listHere[k] for k in [i * 3 + j for i in pickList for j in range(3)]]
+
+
+# print(sfPick(range(75)))
 
 # dataDir = r"../openposeOutput/json"
 def readPoseData(dataDir: str) -> List[List[float]]:
@@ -12,10 +37,45 @@ def readPoseData(dataDir: str) -> List[List[float]]:
         file = open(rf"{dataDir}/{fileName}", mode='r')
         data = json.load(file)
         if len(data["people"]) > 0:
-            objectDataList.append(data["people"][0]["pose_keypoints_2d"])
+            personData = sfPick(data["people"][0]["pose_keypoints_2d"])
+            personX = personData[0:len(personData):3]
+            personY = personData[1:len(personData):3]
+            personConf = personData[2:len(personData):3]
+            filteredPersonCoordList = []
+            filteredPersonIndexList = []
+            for i in range(len(personConf)):
+                if personConf[i] != 0:
+                    filteredPersonCoordList.append(personX[i])
+                    filteredPersonCoordList.append(personY[i])
+                    filteredPersonIndexList.append(i)
+            pMax = max(filteredPersonCoordList)
+            pMin = min(filteredPersonCoordList)
+            for i in filteredPersonIndexList:
+                personX[i] = (personX[i] - pMin) / (pMax - pMin)
+                personY[i] = (personY[i] - pMin) / (pMax - pMin)
+                personData[i * 3] = personX[i]
+                personData[i * 3 + 1] = personY[i]
+            objectDataList.append(personData)
         else:
-            objectDataList.append([0 for i in range(75)])
+            objectDataList.append([0 for _ in range(ListLength)])
+        file.close()
 
     print(f"总数据文件数：{len(dataNameList)}，有效非0数据文件数：{len(objectDataList)}")
+
     # print(objectDataList)
     return objectDataList
+
+
+# "./mark.txt"
+def readMark(path: str) -> List[float]:
+    with open(path, "r") as file:
+        markList = list(file.read())
+        markList = [markDict[i] for i in markList]
+        countList = [0 for i in range(7)]
+        for i in markList:
+            countList[i] += 1
+        file.close()
+    markMatrix = [[1 if j == i else 0 for j in range(7)] for i in markList]
+    print(countList)
+    print(torch.sum(torch.tensor(markMatrix, dtype=float)))
+    return markMatrix, markList
