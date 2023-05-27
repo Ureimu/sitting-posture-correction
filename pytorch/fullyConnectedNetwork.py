352e0cb52a7fa4687133a2af30e057e5fcc202ff
Ureimu <steam1090693441@163.com>
2023-05-27 17:49:10 +0800

    feat: add ui

--- pytorch/fullyConnectedNetwork.py
+++ pytorch/fullyConnectedNetwork.py
@@ -7,36 +7,36 @@ from readData import readData
 import numpy
 
 networkDepth = 4  # 网络深度
-networkSizeList = [75, 1000, 1800, 1200, 7]
+# [75, 1000, 1800, 1200, 7]
+networkSizeList = [75, 100, 180, 120, 7]
 useExponent = False  # 是否使用指数参数控制
 exponent = 0.7  # 指数参数，控制网络每层的值是偏大还是偏小。
-trainSetRate = 0.6  # 测试集占总数据集的比例
 outputTensorLength = 7  # 7分类问题。
 learning_rate = 5  # 学习率
-epochNum = 100000  # 训练轮数, 40000
+epochNum = 40000  # 训练轮数, 40000
 # 每隔step_size个epoch，学习率 x gamma
 step_size = 50
 gamma = 1
 
 printAcc = True  # 是否打印acc参数
-epochReportPercentage = 0.001  # acc参数在每完成百分之多少的时候输出
+epochReportPercentage = 0.1  # acc参数在每完成百分之多少的时候输出
 
 model_store_path = "./model"  # 模型存储路径
 
 trainMarkList, trainMatrix, trainPoseData, testMarkList, testMatrix, testPoseData, indexList = readData("./mark.txt",
                                                                                                         "../openposeOutput/train/json",
-                                                                                                        trainSetRate,
+                                                                                                        1,
                                                                                                         True)
 trainClassNumList = []
 testClassNumList = []
 for i in range(outputTensorLength):
     trainClassNumList.append(sum([1 if i == j else 0 for j in trainMarkList]))
     testClassNumList.append(sum([1 if i == j else 0 for j in testMarkList]))
-print(f"train data class num {trainClassNumList}, test data class num {testClassNumList}")
+print(f"train data class num {trainClassNumList}")
 # print(poseData)
 singlePoseDataLength = len(trainPoseData[0])
 
-print(f"poseLength:{singlePoseDataLength}, trainDataLength:{len(trainPoseData)}, testDataLength:{len(testPoseData)}")
+print(f"poseLength:{singlePoseDataLength}, trainDataLength:{len(trainPoseData)}")
 
 
 # 定义模型
@@ -78,111 +78,97 @@ class Net(nn.Module):
         return netX
 
 
-# 定义一个空列表，用于存储每一次迭代的损失函数值
-losses = []
-accList = []
-accXList = []
-accTestList = []
+if __name__ == '__main__':
 
-net = Net()
+    # 定义一个空列表，用于存储每一次迭代的损失函数值
+    losses = []
+    accList = []
+    accXList = []
+    accTestList = []
 
-# 定义数据
-x = torch.tensor(trainPoseData, dtype=torch.float)
-y = torch.tensor(trainMatrix, dtype=torch.float)
-# print(testMatrix)
-xTest = torch.tensor(testPoseData,
-                     dtype=torch.float)
-yTest = torch.tensor(testMatrix, dtype=torch.float)
-# print(y)
+    net = Net()
 
-maxClassNum = max(trainClassNumList)
-weights = torch.FloatTensor([maxClassNum / i for i in trainClassNumList])  # 类别权重，与样本数成反比
-# 定义损失函数和优化器
+    # 定义数据
+    x = torch.tensor(trainPoseData, dtype=torch.float)
+    y = torch.tensor(trainMatrix, dtype=torch.float)
+    # print(testMatrix)
+    xTest = torch.tensor(testPoseData,
+                         dtype=torch.float)
+    yTest = torch.tensor(testMatrix, dtype=torch.float)
+    # print(y)
 
-criterion = nn.CrossEntropyLoss(weights)
-optimizer = optim.SGD(net.parameters(), lr=learning_rate)
+    # maxClassNum = max(trainClassNumList)
+    # weights = torch.FloatTensor([maxClassNum / i for i in trainClassNumList])  # 类别权重，与样本数成反比
+    # 定义损失函数和优化器
+    # criterion = nn.CrossEntropyLoss(weights)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=learning_rate)
 
-# 定义学习率更新。该方法是：每隔step_size个epoch，学习率 x gamma
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
-# 训练模型
+    # 定义学习率更新。该方法是：每隔step_size个epoch，学习率 x gamma
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
+    # 训练模型
 
-for epoch in range(1, epochNum + 1):
-    optimizer.zero_grad()
-    outputs = net(x)
-    loss = criterion(outputs, y.argmax(dim=1))
-    lossNum = loss.item()
-    losses.append(lossNum)
-    loss.backward()
-    optimizer.step()
-    scheduler.step()
-    if epoch % int(epochNum * epochReportPercentage) == 0 or epoch == epochNum:
-        # print(outputs)
-        outClass = torch.argmax(outputs, 1)
-        yClass = torch.argmax(y, 1)
-        # print(outClass,y)
-        acc = sum([1 if yClass[i] == outClass[i] else 0 for i in range(len(y))]) / len(y)
-        accList.append(acc)
-        accXList.append(epoch)
-
-        # Evaluate test set accuracy
-        test_outputs = net(xTest)
-        test_outClass = torch.argmax(test_outputs, 1)
-        test_yClass = torch.argmax(yTest, 1)
-        test_acc = sum([1 if test_yClass[i] == test_outClass[i] else 0 for i in range(len(yTest))]) / len(yTest)
-        accTestList.append(test_acc)
-
-        if printAcc:
-            print(f"epoch:{epoch} {'%3.2f' % (epoch / epochNum * 100)}% loss:{lossNum} acc:{acc}")
-            print(f"Test set accuracy: {test_acc}")
-
-# 绘制损失函数值随着迭代次数的变化图表
-fig = plt.figure(figsize=(6, 6))
-
-ax1 = fig.add_subplot(111)
-ax1.plot(losses, color="r", label="loss")
-ax1.set_ylabel("loss")
-ax1.set_xlabel("epoch")
-
-# 绘制在训练集上的准确率随着迭代次数的变化图表
-ax2 = ax1.twinx()
-ax2.plot(accXList, accList, color="b", label="train acc")
-ax2.plot(accXList, accTestList, color="g", label="test acc")
-ax2.set_xlabel("Same")
-ax2.set_ylabel("accuracy on train set")
-
-fig.legend(labels=('loss', 'train acc', "test acc"), loc="upper right")
+    for epoch in range(1, epochNum + 1):
+        optimizer.zero_grad()
+        outputs = net(x)
+        loss = criterion(outputs, y.argmax(dim=1))
+        lossNum = loss.item()
+        losses.append(lossNum)
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+        if epoch % int(epochNum * epochReportPercentage) == 0 or epoch == epochNum:
+            # print(outputs)
+            outClass = torch.argmax(outputs, 1)
+            yClass = torch.argmax(y, 1)
+            # print(outClass,y)
+            acc = sum([1 if yClass[i] == outClass[i] else 0 for i in range(len(y))]) / len(y)
+            accList.append(acc)
+            accXList.append(epoch)
 
 
-def percentageFormatter(tmp, pos):
-    return '%3.1f' % (tmp * 100) + "%"
+            if printAcc:
+                print(f"epoch:{epoch} {'%3.2f' % (epoch / epochNum * 100)}% loss:{lossNum} acc:{acc}")
+
+    # 绘制损失函数值随着迭代次数的变化图表
+    fig = plt.figure(figsize=(6, 6))
+
+    ax1 = fig.add_subplot(111)
+    ax1.plot(losses, color="r", label="loss")
+    ax1.set_ylabel("loss")
+    ax1.set_xlabel("epoch")
+
+    # 绘制在训练集上的准确率随着迭代次数的变化图表
+    ax2 = ax1.twinx()
+    ax2.plot(accXList, accList, color="b", label="acc")
+    ax2.set_xlabel("Same")
+    ax2.set_ylabel("accuracy")
+
+    fig.legend(labels=('loss', 'acc'), loc="upper right")
 
 
-ax2.yaxis.set_major_formatter(FuncFormatter(percentageFormatter))
+    def percentageFormatter(tmp, pos):
+        return '%3.1f' % (tmp * 100) + "%"
 
-plt.show()
 
-fig.savefig('./chart/trainLossChart.png')
+    ax2.yaxis.set_major_formatter(FuncFormatter(percentageFormatter))
 
-# print(output_tensor)
+    plt.show()
 
-with torch.no_grad():
-    output_tensor = net(x)
-    pred_probab = nn.Softmax(dim=1)(output_tensor)
-    y_pred = pred_probab.argmax(1)
-    # print(f"trainList Predicted class: {y_pred}")
-    # print(f"trainList mark class: {trainMarkList}")
-    print(
-        f"trainSet accuracy: {sum([1 if y_pred[i] == trainMarkList[i] else 0 for i in range(len(y_pred))]) / len(y_pred)}")
+    fig.savefig('./chart/trainLossChart.png')
 
-    output_tensor = net(xTest)
-    pred_probab = nn.Softmax(dim=1)(output_tensor)
-    y_pred = pred_probab.argmax(1)
-    print(f"testList Predicted class: {y_pred}")
-    print(f"testList mark class: {testMarkList}")
-    print(
-        f"testSet accuracy: {sum([1 if y_pred[i] == testMarkList[i] else 0 for i in range(len(y_pred))]) / len(y_pred)}")
+    # print(output_tensor)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"training finished on {device}")
+    with torch.no_grad():
+        output_tensor = net(x)
+        pred_probab = nn.Softmax(dim=1)(output_tensor)
+        y_pred = pred_probab.argmax(1)
+        # print(f"trainList Predicted class: {y_pred}")
+        # print(f"trainList mark class: {trainMarkList}")
+        print(
+            f"trainSet accuracy: {sum([1 if y_pred[i] == trainMarkList[i] else 0 for i in range(len(y_pred))]) / len(y_pred)}")
 
-torch.save(net.state_dict(), f'{model_store_path}/net_model.ckpt')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"training finished on {device}")
+
+    torch.save(net.state_dict(), f'{model_store_path}/net_model.ckpt')
---
+++ pytorch/predict.py
@@ -0,0 +1,91 @@
+from fullyConnectedNetwork import Net
+import torch
+from PIL import Image
+from torchvision.transforms import transforms
+from torch.autograd import Variable
+import time
+
+# From Python
+# It requires OpenCV installed for Python
+import sys
+import cv2
+import os
+from sys import platform
+
+
+try:
+    # Import Openpose (Windows/Ubuntu/OSX)
+    # os.path.dirname(os.path.realpath(__file__))
+    # openPose的位置
+    openPose_path = r"C:/Users/a1090/Documents/GitHub/openpose"
+    try:
+        # Windows Import
+        if platform == "win32":
+            # Change these variables to point to the correct folder (Release/x64 etc.)
+            sys.path.append(rf'{openPose_path}/build/python/openpose/Release')
+            os.environ['PATH'] = os.environ['PATH'] + ';' + rf'{openPose_path}/build/x64/Release;' + rf'{openPose_path}/build/bin;'
+            import pyopenpose as op
+        else:
+            print(f"Error: not implemented on the {platform} platform, stop importing")
+    except ImportError as e:
+        print(
+            'Error: OpenPose library could not be found. Did you enable `BUILD_PYTHON` in CMake and have this Python '
+            'script in the right folder?')
+        raise e
+
+    # Custom Params (refer to include/openpose/flags.hpp for more parameters)
+    params = {
+        "model_folder": rf"{openPose_path}\models",  # model位置
+        "image_dir":    r"C:\Users\a1090\Documents\GitHub\sitting-posture-correction\pytorch\trainSet",  # 输入目录
+        "disable_blending": True,  # 在黑色背景上绘制骨骼坐标点。
+        "write_images": r"./openposeOutput/train",  # 输出目录
+        "render_threshold": 0.001,  # 控制骨骼关节点最低置信度
+        "number_people_max": 1,  # 识别人数
+        "write_json": r"./openposeOutput/train/json",  # JSON输出目录
+        "keypoint_scale": 3  # `3` to scale it in the range [0,1], where (0,0) would be the top-left corner of the
+        # image, and (1,1) the bottom-right one
+
+    }
+
+    poseModel = op.PoseModel.BODY_25
+    print(op.getPoseBodyPartMapping(poseModel))
+    print(op.getPoseNumberBodyParts(poseModel))
+    print(op.getPosePartPairs(poseModel))
+    print(op.getPoseMapIndex(poseModel))
+
+    # Starting OpenPose
+    opWrapper = op.WrapperPython(op.ThreadManagerMode.Synchronous)
+    opWrapper.configure(params)
+    opWrapper.execute()
+except Exception as e:
+    print(e)
+    sys.exit(-1)
+
+
+def predict_result(datas=None):
+    """
+    :param datas: list
+    :return: int
+    """
+    if datas is None:
+        datas = []
+    model = Net()
+    model.load_state_dict(torch.load(
+        "../model_pth/23classification_eigenvalue.pth", map_location='cpu'))
+    predict = model(Variable(torch.Tensor([datas]).float())).detach().cpu().numpy().tolist()[0]
+    predict = predict.index(max(predict))
+
+    return predict
+
+
+if __name__ == '__main__':
+    start = time.process_time()
+    data = [0.0, 163115.3545813507, 135018.25825455785, 12331.515849550487, 120673.87059219554, 6122.537879968295, 0.0,
+            163115.3545813507, 163115.3545813507, 0.0, 0.0, 433081.6202105442, 163098.4398608161, 0.0,
+            163115.3545813507, 0.4150879533697161, 0.9778420883296507, 0, 0, -0.8914324274471591, -0.2886839472216833,
+            0, 0, 0, -0.6680837068163037, 0, 0, 0, 1.0, 0.5246781242226066]
+    # print(predict_result(data))
+    predict_result(data)
+
+    end = time.process_time()
+    print(end - start)
